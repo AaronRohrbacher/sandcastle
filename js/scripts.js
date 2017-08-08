cca463dd5bf12fac27ff98e95b948cf405433763
Aaron Rohrbacher and Senya Webster <aaron@>
2017-08-08 10:58:16 -0700

    add background changing buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
-
 $(document).ready(function() {
+  $("button#white").click(function() {
+    $("body").removeClass();
+    $("body").addClass("whiteBackground");
+  });
+  $("button#black").click(function(){
+    $("body").removeClass();
+    $("body").addClass("blackBackground");
+  });
   $(".clickable").click(function() {
-    $(".hidden").slideDown();
+    $(".hidden").toggle();
   });
 });
